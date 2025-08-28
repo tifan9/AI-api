91dcbf87629794a05ecb86c207cf28c6a7a04250
uwase sophie <uwasesophie91@gmail.com>
2025-08-28 08:22:56 +0200

    fix api key

--- pages/api/botapi.js
+++ pages/api/botapi.js
@@ -2,7 +2,7 @@ const { DiscussServiceClient } = require("@google-ai/generativelanguage");
 const { GoogleAuth } = require("google-auth-library");
 
 const MODEL_NAME = "models/chat-bison-001";
-const API_KEY = "AIzaSyAt2CPCMZ975OzkDTVc2Qhxqp9FOWJVpgs"
+const API_KEY = process.env.API_KEY
 
 const client = new DiscussServiceClient({
   authClient: new GoogleAuth().fromAPIKey(API_KEY),
